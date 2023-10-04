388a058b5b8abeccc9dd57621a0e70a0ef93c55a
wawa9149 <wawa9149@holamago.com>
2023-10-04 11:09:03 +0900

    Add

--- flutter_sttapp/lib/main.dart
+++ flutter_sttapp/lib/main.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
+import 'package:social_media_recorder/audio_encoder_type.dart';
+import 'package:social_media_recorder/screen/social_media_recorder.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const MyApp());
 }
 
@@ -11,100 +12,53 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title: 'Flutter Demo',
-      home: MyHomePage(),
+    return MaterialApp(
+      debugShowMaterialGrid: false,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final SpeechToText _speechToText = SpeechToText();
-  bool _speechEnabled = false;
-  String _lastWords = '';
-
   @override
   void initState() {
     super.initState();
-    _initSpeech();
-  }
-
-  void _initSpeech() async {
-    _speechEnabled = await _speechToText.initialize(
-      onError: (error) => print('Error: $error'),
-    );
-    setState(() {});
-  }
-
-  void _startListening() async {
-    await _speechToText.listen(
-      onResult: _onSpeechResult,
-      listenFor: const Duration(seconds: 10), // 녹음 시간 설정
-    );
-    setState(() {});
-  }
-
-  void _stopListening() async {
-    await _speechToText.stop();
-    setState(() {});
-  }
-
-  void _onSpeechResult(SpeechRecognitionResult result) {
-    setState(() {
-      _lastWords = result.recognizedWords;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Speech Demo'),
-      ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(16),
-              child: const Text(
-                'Recognized words:',
-                style: TextStyle(fontSize: 20.0),
-              ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 140, left: 4, right: 4),
+          child: Align(
+            alignment: Alignment.centerRight,
+            child: SocialMediaRecorder(
+              // maxRecordTimeInSecond: 5,
+              startRecording: () {
+                // function called when start recording
+              },
+              stopRecording: (time) {
+                // function called when stop recording, return the recording time
+              },
+              sendRequestFunction: (soundFile, time) {
+                //  print("the current path is ${soundFile.path}");
+              },
+              encode: AudioEncoderType.AAC,
             ),
-            Expanded(
-              child: Container(
-                padding: const EdgeInsets.all(16),
-                child: Text(
-                  _speechToText.isListening
-                      ? _lastWords
-                      : _speechEnabled
-                          ? 'Tap the microphone to start listening...'
-                          : 'Speech not available',
-                ),
-              ),
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _speechToText.isListening ? _stopListening : _startListening,
-        tooltip: 'Listen',
-        child: Icon(_speechToText.isListening ? Icons.mic_off : Icons.mic),
-      ),
     );
-  }
-
-  @override
-  void dispose() {
-    _speechToText.stop(); // 페이지를 벗어나기 전에 녹음 중지
-    super.dispose();
   }
 }
