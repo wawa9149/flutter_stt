11d963219a1ba3ca0cdff1a3cbd61ceb329b1ad5
wawa9149 <wawa9149@holamago.com>
2023-10-05 16:10:34 +0900

    record

--- flutter_sttapp/lib/main.dart
+++ flutter_sttapp/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
-import 'dart:io';
 import 'package:path_provider/path_provider.dart';
+
+//import 'package:googleapis/speech/v1.dart';
 
 void main() => runApp(const MyApp());
 
@@ -55,9 +56,8 @@ class _MyHomePageState extends State<MyHomePage> {
         _isRecording = true;
         _recordingPath = filePath;
       });
-    } catch (e) {
-      print('Error: $e');
-    }
+      // ignore: empty_catches
+    } catch (e) {}
   }
 
   Future<void> _stopRecording() async {
@@ -69,6 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> _playRecording() async {
+    await _player.openPlayer();
     await _player.startPlayer(
       fromURI: _recordingPath,
       codec: Codec.aacADTS,
@@ -81,6 +82,45 @@ class _MyHomePageState extends State<MyHomePage> {
     _recorder.closeRecorder();
     super.dispose();
   }
+
+//   void transcribeAudio(String audioFilePath) async {
+//     // 서비스 계정 키의 경로
+//     const serviceAccountPath = 'path/to/your/service-account-key.json';
+
+//     // 서비스 계정 로드
+//     final serviceAccountCredentials =
+//         ServiceAccountCredentials.fromJson(File(serviceAccountPath).readAsStringSync());
+
+//     // Speech-to-Text API에 액세스하기 위한 사용자 인증
+//     final client = await clientViaServiceAccount(serviceAccountCredentials, speech.SpeechApi.speechScope);
+
+//     // Speech-to-Text API 클라이언트 생성
+//     final speechApi = speech.SpeechApi(client);
+
+//     // Speech Recognition 요청 구성
+//     final config = speech.RecognitionConfig()
+//       ..encoding = 'LINEAR16' // 오디오 인코딩 형식
+//       ..sampleRateHertz = 16000 // 샘플 속도
+//       ..languageCode = 'en-US'; // 언어 코드
+
+//     // 오디오 파일에서 Speech Recognition 수행
+//     final audioData = File(audioFilePath).readAsBytesSync();
+//     final response = await speechApi.speech.recognize(
+//       speech.SpeechRecognitionRequest()..config = config..audio = speech.SpeechRecognitionAudio()..content = audioData,
+//     );
+
+//     // 결과 출력
+//     if (response.results != null && response.results!.isNotEmpty) {
+//       final transcript = response.results!.first.alternatives!.first.transcript;
+//       print('Transcription: $transcript');
+//     } else {
+//       print('No transcription results found.');
+//     }
+
+//     // 클라이언트 종료
+//     client.close();
+//   }
+// }
 
   @override
   Widget build(BuildContext context) {
@@ -95,6 +135,8 @@ class _MyHomePageState extends State<MyHomePage> {
             _isRecording
                 ? ElevatedButton(
                     onPressed: _stopRecording,
+                    //transcribeAudio('path/to/your/audio/file.wav'),
+
                     child: const Text('Stop Recording'),
                   )
                 : ElevatedButton(
@@ -108,6 +150,10 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: const Text('Play Recording'),
                   )
                 : Container(),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text("녹음 내용")
           ],
         ),
       ),
